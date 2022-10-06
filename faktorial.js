649f8e7ab92c3d57e62715047581ddd3be39dd35
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-10-06 14:21:09 +0700

    faktorial ternary

--- faktorial.js
+++ faktorial.js
@@ -1,9 +1,15 @@
 const faktorial = (n) => {
-	if (n == 0) {
-		return 1;
-	}else {
-		return n * faktorial(n-1)
-	}
+	
+	//factorial recursive example
+	// if (n == 0) {
+	// 	return 1;
+	// }else{
+	// 	return n * faktorial(n-1)
+	// }
+
+	//using ternary operator
+	return((n == 0) ? (1): n * faktorial(n - 1));
+
 }
 
 console.log(faktorial(5)); 
